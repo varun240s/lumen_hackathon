ce1644267499b1e807a0b62265914ba78a185b22
varun240s <reddyvarun240@gmail.com>
2025-09-09 19:58:54 +0530

    small updates regarding jwt key.

--- authentication/backend/app/__init__.py
+++ authentication/backend/app/__init__.py
@@ -12,7 +12,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY'),
-        JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY'),
+        JWT_SECRET_KEY=os.environ.get('SECRET_KEY'),
         SQLALCHEMY_DATABASE_URI='sqlite:///yourdb.db',
     )
 
